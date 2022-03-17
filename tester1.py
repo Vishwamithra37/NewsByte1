96bad7b7428057235e8490709dc92c87e1563bb1
Vishwa Mithra <mithravishwa37@gmail.com>
2022-03-18 05:06:49 +0530

    got the host waiting for cllarity

--- tester1.py
+++ tester1.py
@@ -1,18 +1,24 @@
-# from urllib import request
+from traceback import print_tb
 from config import *
+
+
 #############################Initialized variables############################
 utm_array=["utm_source","utm_medium","utm_campaign","utm_term","utm_content"]
 #############################Initialized variables#############################
 
-@app.route('/')
+@app.route('/index')
 def index():
     # Get all args in the url.
     args = flask.request.args.to_dict()
     # Get the url.
     url = flask.request.url
+    # Get the host part of the url.
+    host = flask.request.host
+    print(host)
     print(args)
     for i in utm_array:
         print(flask.request.args.get(i))
+        
     return "Hello World!"+"<br>"+url    
 docs.register(target=index)
 
