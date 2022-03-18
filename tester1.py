b67129fb0aa9f6e2c12d67c6e36b89d367a6211b
Vishwa Mithra <mithravishwa37@gmail.com>
2022-03-18 14:27:09 +0530

    compressor

--- tester1.py
+++ tester1.py
@@ -1,17 +1,15 @@
 from traceback import print_tb
 from config import *
 from dbconfig import *
+import hashlib
 
 @app.route('/compressor',methods=['POST'])
 def index():
     dat= flask.request.get_json()
     url = dat['url']
     assert len(url.split('\\'))==1
-    
-
-
-
-
+    hashurl=hashlib.sha224(url.encode('utf-8')).hexdigest()
+    hashurl=hashurl[:6] # truncate to 6 chars.
 
     return "<h1>Welcome to NewsBytes_VishwaMithra</h1><br><br><h2>Please visit "+url
 docs.register(target=index)
